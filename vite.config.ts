1379df2fdc4d886a14f76b7cd0f3631b29c023e9
Ji Sang Seo <inaki96@naver.com>
2023-07-16 03:03:58 +0900

    :feat types for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     minify: false,
     lib: {
